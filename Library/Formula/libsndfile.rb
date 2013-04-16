6785d6f71206bae12b23675b5b2b976074a83fc9
Yoshiki Yazawa <yaz@honeyplanet.jp>
2013-04-16 14:24:43 +0900

    handle more metadata in wav format

--- Library/Formula/libsndfile.rb
+++ Library/Formula/libsndfile.rb
@@ -12,6 +12,9 @@ class Libsndfile < Formula
   def patches
     # libsndfile doesn't find Carbon.h using XCode 4.3:
     # fixed upstream: https://github.com/erikd/libsndfile/commit/d04e1de82ae0af48fd09d5cb09bf21b4ca8d513c
+    #
+    # yaz added the genre patch for wav
+    #
     DATA
   end
 
@@ -34,3 +37,33 @@ __END__
  	#include <CoreAudio/AudioHardware.h>
  
  #elif defined (HAVE_SNDIO_H)
+--- a/programs/sndfile-metadata-get.c	2011-03-21 08:06:59.000000000 +0900
++++ b/programs/sndfile-metadata-get.c	2012-07-05 23:05:37.104941613 +0900
+@@ -120,6 +120,7 @@ usage_exit (const char *progname, int ex
+ 		"    --str-date            Print the creation date metadata.\n"
+ 		"    --str-album           Print the album metadata.\n"
+ 		"    --str-license         Print the license metadata.\n"
++		"    --str-genre           Print the genre metadata.\n"
+ 		) ;
+ 
+ 	printf ("Using %s.\n\n", sf_version_string ()) ;
+@@ -164,6 +165,7 @@ process_args (SNDFILE * file, const SF_B
+ 		HANDLE_STR_ARG ("--str-date", "Create date", SF_STR_DATE) ;
+ 		HANDLE_STR_ARG ("--str-album", "Album", SF_STR_ALBUM) ;
+ 		HANDLE_STR_ARG ("--str-license", "License", SF_STR_LICENSE) ;
++		HANDLE_STR_ARG ("--str-genre", "Genre", SF_STR_GENRE) ;
+ 
+ 		if (! do_all)
+ 		{	printf ("Error : Don't know what to do with command line arg '%s'.\n\n", argv [k]) ;
+--- a/src/wav.c	2011-06-21 19:10:01.000000000 +0900
++++ b/src/wav.c	2012-07-05 22:44:24.739710197 +0900
+@@ -1423,6 +1423,9 @@ wav_subchunk_parse (SF_PRIVATE *psf, int
+ 			case INAM_MARKER :
+ 					psf_store_string (psf, SF_STR_TITLE, psf->u.cbuf) ;
+ 					break ;
++			case IPRD_MARKER :
++					psf_store_string (psf, SF_STR_ALBUM, psf->u.cbuf) ;
++					break ;
+ 			case IART_MARKER :
+ 					psf_store_string (psf, SF_STR_ARTIST, psf->u.cbuf) ;
+ 					break ;
