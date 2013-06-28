9c471e9b78c47ca7d4ed321ebafd4c993ce2c741
Yoshiki Yazawa <yaz@honeyplanet.jp>
2013-06-28 18:15:10 +0900

    use previous version for work around incompatibilities.

--- Library/Formula/gtkmm.rb
+++ Library/Formula/gtkmm.rb
@@ -2,9 +2,8 @@ require 'formula'
 
 class Gtkmm < Formula
   homepage 'http://www.gtkmm.org/'
-  url 'http://ftp.gnome.org/pub/GNOME/sources/gtkmm/2.24/gtkmm-2.24.3.tar.xz'
-  sha256 'c564a438677174b97d69dd70467cb03c933481006398dc9377417aa6abe02a39'
-
+  url 'http://ftp.gnome.org/pub/GNOME/sources/gtkmm/2.24/gtkmm-2.24.2.tar.xz'
+  sha256 'c90c7a29c20dd6d795925bd66ad1ff40be07cea0e4c03f4d6105a8c07448953d'
   depends_on 'xz' => :build
   depends_on 'pkg-config' => :build
   depends_on 'glibmm'
