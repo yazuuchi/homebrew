1d7db86a3257d9032bfb82e86dd37bfb9a71b79a
Yoshiki Yazawa <yaz@honeyplanet.jp>
2014-01-09 17:44:48 +0900

    drop -lIOKit

--- Library/Formula/libhid.rb
+++ Library/Formula/libhid.rb
@@ -8,6 +8,8 @@ class Libhid < Formula
   depends_on 'libusb'
   depends_on 'libusb-compat'
 
+  def patches; DATA; end
+
   def install
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
@@ -16,3 +18,16 @@ class Libhid < Formula
     system "make install"
   end
 end
+
+__END__
+--- a/src/Makefile.in   Thu Jan 09 17:29:34 2014 +0900
++++ b/src/Makefile.in   Thu Jan 09 17:35:58 2014 +0900
+@@ -39,7 +39,7 @@ POST_UNINSTALL = :
+ build_triplet = @build@
+ host_triplet = @host@
+ @OS_BSD_FALSE@@OS_DARWIN_TRUE@@OS_LINUX_FALSE@@OS_SOLARIS_FALSE@am__append_1 = -no-cpp-precomp
+-@OS_BSD_FALSE@@OS_DARWIN_TRUE@@OS_LINUX_FALSE@@OS_SOLARIS_FALSE@am__append_2 = -lIOKit -framework "CoreFoundation"
++@OS_BSD_FALSE@@OS_DARWIN_TRUE@@OS_LINUX_FALSE@@OS_SOLARIS_FALSE@am__append_2 = -framework "CoreFoundation"
+ bin_PROGRAMS = libhid-detach-device$(EXEEXT)
+ subdir = src
+ DIST_COMMON = $(include_HEADERS) $(srcdir)/Makefile.am \
