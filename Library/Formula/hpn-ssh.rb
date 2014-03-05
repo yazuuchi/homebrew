5afff88aaafdf98f12438519a9cc6b5668c8c5a8
Yoshiki Yazawa <yaz@honeyplanet.jp>
2014-03-05 11:31:39 +0900

    added hpn-ssh.rb

---
+++ Library/Formula/hpn-ssh.rb
@@ -0,0 +1,36 @@
+require 'formula'
+
+class HpnSsh < Formula
+  homepage 'http://www.openssh.com/'
+  url 'http://ftp5.eu.openbsd.org/ftp/pub/OpenBSD/OpenSSH/portable/openssh-6.5p1.tar.gz'
+  version '6.5p1'
+  sha1 '3363a72b4fee91b29cf2024ff633c17f6cd2f86d'
+
+  option 'with-brewed-openssl', 'Build with Homebrew OpenSSL instead of the system version'
+
+  depends_on 'openssl' if build.with? 'brewed-openssl'
+  depends_on 'ldns' => :optional
+  depends_on 'pkg-config' => :build if build.with? "ldns"
+
+  conflicts_with 'openssh'
+
+  def patches
+    'http://www.honeyplanet.jp/openssh_65_hpn.diff'
+  end
+
+  def install
+
+    args = %W[
+      --with-libedit
+      --prefix=#{prefix}
+    ]
+
+    args << "--with-ssl-dir=#{Formula.factory('openssl').opt_prefix}" if build.with? 'brewed-openssl'
+    args << "--with-ldns" if build.with? "ldns"
+    args << "--without-openssl-header-check"
+
+    system "./configure", *args
+    system "make"
+    system "make install"
+  end
+end
