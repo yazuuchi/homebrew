d4a2185a71add0289772110926b9a910ea5d8c51
Yoshiki Yazawa <yaz@honeyplanet.jp>
2014-05-26 13:41:41 +0900

    libpano: now it can be built from HEAD

--- Library/Formula/libpano.rb
+++ Library/Formula/libpano.rb
@@ -2,20 +2,23 @@ require 'formula'
 
 class Libpano < Formula
   homepage 'http://panotools.sourceforge.net/'
-#  url 'https://downloads.sourceforge.net/project/panotools/libpano13/libpano13-2.9.18/libpano13-2.9.18.tar.gz'
-#  version '13-2.9.18'
-#  sha1 '23849bdbdfc9176a2b53d157e58bd24aa0e7276e'
-#  revision 1
-
   url 'https://downloads.sourceforge.net/project/panotools/libpano13/libpano13-2.9.19/libpano13-2.9.19_beta1.tar.gz'
   version '13-2.9.19_beta1'
   sha1 '695d6b26112ee18f3783b826e8c1c645f1b4ad2b'
+
+  head 'http://hg.code.sf.net/p/panotools/libpano13', :using => :hg
 
   depends_on 'libpng'
   depends_on 'jpeg'
   depends_on 'libtiff'
 
   def install
+    if build.head?
+      # Link the Mercurial repository into the download directory so
+      # build.py can use it to figure out a version number.
+      ln_s cached_download + ".hg", ".hg"
+      system "autoreconf -fiv"
+    end
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--mandir=#{man}"
