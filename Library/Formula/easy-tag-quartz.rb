b7ef2231c850bc6a2c3d39fe9cc27e1912a5cb26
Yoshiki Yazawa <yaz@honeyplanet.jp>
2014-08-20 09:26:52 +0900

    easy-tag-quartz: make use of quartz backended gtk+

---
+++ Library/Formula/easy-tag-quartz.rb
@@ -0,0 +1,35 @@
+require "formula"
+
+class EasyTagQuartz < Formula
+  homepage "http://projects.gnome.org/easytag"
+  url "http://ftp.gnome.org/pub/GNOME/sources/easytag/2.2/easytag-2.2.3.tar.xz"
+  sha1 "dfe4170f01b0d1e571329999995b5859fb2a2ce3"
+
+  depends_on :x11
+  depends_on "pkg-config" => :build
+  depends_on "intltool" => :build
+  depends_on "itstool" => :build
+  depends_on "gtk+-quartz"
+  depends_on "pango-quartz"
+  depends_on "cairo-quartz"
+  depends_on "hicolor-icon-theme"
+  depends_on "id3lib"
+  depends_on "libid3tag"
+  depends_on "taglib"
+
+  depends_on "libvorbis" => :recommended
+  depends_on "flac" => :recommended
+  depends_on "libogg" if build.with? "flac"
+
+  depends_on "speex" => :optional
+  depends_on "wavpack" => :optional
+
+  def install
+    ENV.append_path "PYTHONPATH", "#{Formula["libxml2"].lib}/python2.7/site-packages"
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make"
+    ENV.deparallelize # make install fails in parallel
+    system "make install"
+  end
+end
