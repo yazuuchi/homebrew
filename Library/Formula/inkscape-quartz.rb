0507614bb777b869fbba6ca0ad9a5ec9f4eaeac7
Yoshiki Yazawa <yaz@honeyplanet.jp>
2014-08-28 13:21:26 +0900

    add poppler-quartz.rb and inkscape-quartz.rb

---
+++ Library/Formula/inkscape-quartz.rb
@@ -0,0 +1,61 @@
+require 'formula'
+
+class InkscapeQuartz < Formula
+  homepage 'http://inkscape.org/'
+  url 'https://downloads.sourceforge.net/project/inkscape/inkscape/0.48.5/inkscape-0.48.5.tar.gz'
+  sha1 'e14789da0f6b5b84ef26f6759e295bc4be7bd34d'
+
+  bottle do
+    sha1 "284ea91d099a9561e0c87001641539c2a4038baf" => :mavericks
+    sha1 "bafdb00a9a3b23243e2ae03d2ae1155c3ffc246c" => :mountain_lion
+    sha1 "56127cf9ebfe7c476e8b8bb2cb334bc5af4f4296" => :lion
+  end
+
+  head do
+    url 'lp:inkscape/0.48.x', :using => :bzr
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+  end
+
+  depends_on 'pkg-config' => :build
+  depends_on 'intltool' => :build
+  depends_on 'boost-build' => :build
+  depends_on 'gettext'
+  depends_on 'bdw-gc'
+  depends_on 'glibmm'
+  depends_on 'gtkmm-quartz'
+  depends_on 'gsl'
+  depends_on 'boost'
+  depends_on 'popt'
+  depends_on 'little-cms'
+  depends_on 'cairomm-quartz'
+  depends_on 'pango-quartz'
+  depends_on :x11
+  depends_on 'poppler-quartz' => :optional
+  depends_on 'hicolor-icon-theme'
+
+  needs :cxx11
+
+  fails_with :clang do
+    cause "inkscape's dependencies will be built with libstdc++ and fail to link."
+  end if MacOS.version < :mavericks
+
+  def install
+    ENV.cxx11
+
+    system "./autogen.sh" if build.head?
+
+    args = [ "--disable-dependency-tracking",
+             "--prefix=#{prefix}",
+             "--enable-lcms" ]
+    args << "--disable-poppler-cairo" if build.without? "poppler-quartz"
+    system "./configure", *args
+
+    system "make install"
+  end
+
+  test do
+    system "#{bin}/inkscape", "-V"
+  end
+end
---
+++ Library/Formula/poppler-quartz.rb
@@ -0,0 +1,64 @@
+require 'formula'
+
+class PopplerQuartz < Formula
+  homepage 'http://poppler.freedesktop.org'
+  url 'http://poppler.freedesktop.org/poppler-0.26.4.tar.xz'
+  sha1 '26ff3310cb4866829030c73070df4ae9aa4f6cac'
+
+  bottle do
+    sha1 "6634b3885414fddfec33f2070e6b27a63ef63372" => :mavericks
+    sha1 "89893811796e5b7d949364859ac98d98ba436406" => :mountain_lion
+    sha1 "669ae1aeece7b6a643b52a3bfa4e0cc4114b2211" => :lion
+  end
+
+  option 'with-qt4', 'Build Qt backend'
+  option 'with-lcms2', 'Use color management system'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'cairo-quartz'
+  depends_on 'fontconfig'
+  depends_on 'freetype'
+  depends_on 'gettext'
+  depends_on 'glib'
+  depends_on 'gobject-introspection'
+  depends_on 'jpeg'
+  depends_on 'libpng'
+  depends_on 'libtiff'
+  depends_on 'openjpeg'
+
+  depends_on 'qt' if build.with? 'qt4'
+  depends_on 'little-cms2' if build.with? 'lcms2'
+
+  conflicts_with 'pdftohtml', :because => 'both install `pdftohtml` binaries'
+
+  conflicts_with 'pdf2image', 'xpdf',
+    :because => 'poppler, pdf2image, and xpdf install conflicting executables'
+
+  resource 'font-data' do
+    url 'http://poppler.freedesktop.org/poppler-data-0.4.7.tar.gz'
+    sha1 '556a5bebd0eb743e0d91819ba11fd79947d8c674'
+  end
+
+  def install
+    args = %W[
+      --disable-dependency-tracking
+      --prefix=#{prefix}
+      --enable-xpdf-headers
+      --enable-poppler-glib
+      --disable-gtk-test
+      --enable-introspection=yes
+    ]
+
+    if build.with? "qt4"
+      args << "--enable-poppler-qt4"
+    else
+      args << "--disable-poppler-qt4"
+    end
+
+    args << "--enable-cms=lcms2" if build.with? "lcms2"
+
+    system "./configure", *args
+    system "make install"
+    resource('font-data').stage { system "make", "install", "prefix=#{prefix}" }
+  end
+end
