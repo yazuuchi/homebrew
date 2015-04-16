ca1848407419c65d9b8c20357f964fddf2fbbdaf
Yoshiki Yazawa <yaz@honeyplanet.jp>
2015-04-16 20:46:40 +0900

    emacs-quartz: update 24.5

--- Library/Formula/emacs-quartz.rb
+++ Library/Formula/emacs-quartz.rb
@@ -4,27 +4,22 @@ class EmacsQuartz < Formula
   homepage "https://www.gnu.org/software/emacs/"
 
   stable do
-    url "http://ftpmirror.gnu.org/emacs/emacs-24.4.tar.xz"
-    mirror "https://ftp.gnu.org/pub/gnu/emacs/emacs-24.4.tar.xz"
-    sha256 "47e391170db4ca0a3c724530c7050655f6d573a711956b4cd84693c194a9d4fd"
-    # Fix ns-antialias-text, broken in 24.4, from upstream:
-    # https://github.com/emacs-mirror/emacs/commit/604a4d21ead40691afe3efe13f0ba1000b2cd61a
-    # http://debbugs.gnu.org/cgi/bugreport.cgi?bug=18876
-
-    patch do
-      url 'https://gist.githubusercontent.com/scotchi/66edaf426d7375c0f061/raw/4c5229a8a719f81fa6bd2e1e0c85d10b6f635765/emacs-fix-ns-antialias-text-mac-os.patch'
-      sha1 'b63eab599a7ce69de03629494a727f45b310c166'
-    end
+    url "http://ftpmirror.gnu.org/emacs/emacs-24.5.tar.xz"
+    mirror "https://ftp.gnu.org/pub/gnu/emacs/emacs-24.5.tar.xz"
+    sha256 "dd47d71dd2a526cf6b47cb49af793ec2e26af69a0951cc40e43ae290eacfc34e"
   end
-
-  option "cocoa", "Build a Cocoa version of emacs"
-  option "keep-ctags", "Don't remove the ctags executable that emacs provides"
 
   head do
     url "http://git.sv.gnu.org/r/emacs.git"
     depends_on "autoconf" => :build
     depends_on "automake" => :build
   end
+
+  option "with-cocoa", "Build a Cocoa version of emacs"
+  option "with-ctags", "Don't remove the ctags executable that emacs provides"
+
+  deprecated_option "cocoa" => "with-cocoa"
+  deprecated_option "keep-ctags" => "with-ctags"
 
   depends_on "pkg-config" => :build
   depends_on "d-bus" => :optional
@@ -40,32 +35,31 @@ class EmacsQuartz < Formula
   end
 
   def install
-    # HEAD builds blow up when built in parallel as of April 20 2012
-    # FIXME is this still necessary? It's been more than two years, surely any
-    # race conditions would have made it into release by now.
-    ENV.deparallelize unless build.stable?
-
     args = ["--prefix=#{prefix}",
             "--enable-locallisppath=#{HOMEBREW_PREFIX}/share/emacs/site-lisp",
             "--infodir=#{info}/emacs"]
+
     args << "--with-file-notification=gfile" if build.with? "glib"
+
     if build.with? "d-bus"
       args << "--with-dbus"
     else
       args << "--without-dbus"
     end
+
     if build.with? "gnutls"
       args << "--with-gnutls"
     else
       args << "--without-gnutls"
     end
+
     args << "--with-rsvg" if build.with? "librsvg-quartz"
     args << "--with-imagemagick" if build.with? "imagemagick-quartz"
     args << "--without-popmail" if build.with? "mailutils"
 
     system "./autogen.sh" unless build.stable?
 
-    if build.include? "cocoa"
+    if build.with? "cocoa"
       args << "--with-ns" << "--disable-ns-self-contained"
       system "./configure", *args
       system "make"
@@ -80,7 +74,6 @@ class EmacsQuartz < Formula
       EOS
     else
       args << "--without-x"
-
       system "./configure", *args
       system "make"
       system "make", "install"
@@ -88,18 +81,37 @@ class EmacsQuartz < Formula
 
     # Follow MacPorts and don't install ctags from Emacs. This allows Vim
     # and Emacs and ctags to play together without violence.
-    unless build.include? "keep-ctags"
+    if build.without? "ctags"
       (bin/"ctags").unlink
       (man1/"ctags.1.gz").unlink
     end
   end
 
   def caveats
-    if build.include? "cocoa" then <<-EOS.undent
+    if build.with? "cocoa" then <<-EOS.undent
       A command line wrapper for the cocoa app was installed to:
         #{bin}/emacs
       EOS
     end
+  end
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/emacs</string>
+        <string>--daemon</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+    </dict>
+    </plist>
+    EOS
   end
 
   test do
